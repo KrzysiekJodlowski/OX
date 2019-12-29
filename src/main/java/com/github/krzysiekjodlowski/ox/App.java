2376051404e41a365a09e85a867d6e4c3a7c27ab
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-12-29 17:11:03 +0100

    Initialize OX project

---
+++ src/main/java/com/github/krzysiekjodlowski/ox/App.java
@@ -0,0 +1,8 @@
+package com.github.krzysiekjodlowski.ox;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to OX game!");
+    }
+}
---
+++ src/test/java/com/github/krzysiekjodlowski/ox/AppTest.java
@@ -0,0 +1,20 @@
+package com.github.krzysiekjodlowski.ox;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void test() {
+        // arrange
+
+
+        // act
+
+
+        // assert
+        assertTrue(true);
+    }
+}
