9fa71e453655dc372a31de6eb18eafb4211725ba
KrzysiekJodlowski <Krzysztof_Jodlowski@epam.com>
2019-12-30 10:35:49 +0100

    Provide automated scripts
    Description of how they work is provided in readme file.

--- src/main/java/com/github/krzysiekjodlowski/ox/App.java
+++ src/main/java/com/github/krzysiekjodlowski/ox/App.java
@@ -1,5 +1,10 @@
 package com.github.krzysiekjodlowski.ox;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * @author KrzysiekJodlowski
  */
@@ -13,9 +18,40 @@ final class App {
     }
 
     /**
-     * @param args line arguments
+     * In this iteration App checks if there
+     * is any custom input and then creates
+     * game board representation for each case
+     * from that file.
+     * @param args file with custom input
+     * @throws FileNotFoundException when there is no such file
+     * @throws BoardSizeOutOfBoundsException when board size
+     * parameter is too big
      */
-    public static void main(final String[] args) {
+    public static void main(final String[] args)
+            throws FileNotFoundException, BoardSizeOutOfBoundsException {
+        Board board;
+        int boardSize = 3;
+        BoardPrinter boardPrinter = new BoardPrinter();
+
+        if (args.length == 1) {
+            Scanner scanner = new Scanner(
+                    new FileInputStream(new File(args[0]))
+            );
+            while (scanner.hasNextLine()) {
+                boardSize = scanner.nextInt();
+                board = new Board(boardSize);
+                System.out.println(
+                        boardPrinter.getBoardRepresentation(board)
+                );
+            }
+        } else {
+            board = new Board(boardSize);
+            System.out.println(
+                    boardPrinter.getBoardRepresentation(board)
+            );
+        }
+
+
 
     }
 }
