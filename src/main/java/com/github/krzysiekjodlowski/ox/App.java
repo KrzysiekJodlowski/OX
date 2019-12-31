e943f875868b65ea86bc06ff272502e22e256d5a
KrzysiekJodlowski <Krzysztof_Jodlowski@epam.com>
2019-12-31 13:29:21 +0100

    pom.xml and App update
    - update testng dependency version
    - change checkstyle config to google_checks
    - move javadoc description on top of the class
    - handle FileNotFoundException with proper message
    - upgrade while loop condition

--- src/main/java/com/github/krzysiekjodlowski/ox/App.java
+++ src/main/java/com/github/krzysiekjodlowski/ox/App.java
@@ -6,6 +6,12 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
+ * At this moment App checks if there
+ * is any custom input and then creates
+ * game board representation for each case
+ * from that file. Implementation of this
+ * class will change and should not be
+ * concerned when reviewing.
  * @author KrzysiekJodlowski
  */
 final class App {
@@ -18,27 +24,23 @@ final class App {
     }
 
     /**
-     * In this iteration App checks if there
-     * is any custom input and then creates
-     * game board representation for each case
-     * from that file. Implementation of this
-     * method will change and should not be
-     * concerned when reviewing.
-     * @param args file with custom input
-     * @throws FileNotFoundException when there
-     * is no such file parameter is too big
+     * @param args file with custom input (expecting file content as first parameter)
      */
-    public static void main(final String[] args)
-            throws FileNotFoundException {
+    public static void main(final String[] args) {
         Board board;
         int boardSize = 3;
         BoardPrinter boardPrinter = new BoardPrinter(boardSize);
 
-        if (args.length == 1) {
-            Scanner scanner = new Scanner(
-                    new FileInputStream(new File(args[0]))
-            );
-            while (scanner.hasNextInt()) {
+        if (args.length > 0) {
+            Scanner scanner = null;
+            try {
+                scanner = new Scanner(
+                        new FileInputStream(new File(args[0]))
+                );
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found!");
+            }
+            while (scanner != null &&scanner.hasNextInt()) {
                 boardSize = scanner.nextInt();
                 board = new Board(boardSize);
                 boardPrinter = new BoardPrinter(boardSize);
