edc1b15625343083f0316b9505dd3f1a765531e9
KrzysiekJodlowski <Krzysztof_Jodlowski@epam.com>
2020-01-07 16:22:05 +0100

    Scripts update

--- automata/generator.py
+++ automata/generator.py
@@ -1,8 +1,34 @@
 import sys
 
-starting_player = sys.argv[1]
-board_size = sys.argv[2]
-win_condition = sys.argv[3]
+board_size = int(sys.argv[1])
+win_condition = int(sys.argv[2])
+starting_player = sys.argv[3]
 line_type = sys.argv[4]
 
-print(starting_player + " " + board_size + " " + win_condition + " " + line_type)
+player = ""
+valid_input = True
+
+if (board_size < 3 or board_size > 40):
+    print("Wrong board size input! -> {}".format(board_size))
+    valid_input = False
+if (win_condition < 3 or win_condition > board_size * board_size):
+    print("Wrong win condition! -> {}".format(win_condition))
+    valid_input = False
+if (starting_player == "O"):
+    player = 1
+elif (starting_player == "X"):
+    player = 2
+else:
+    print("Wrong starting player! -> {}".format(starting_player))
+    valid_input = False
+
+if valid_input:
+    print('''*** Welcome to OX game! ***
+You can quit the game anytime by pressing ctrl+C.
+Please select board's side length (whole numbers from 3 to 40)
+{}
+Select the length of the winning line (numbers from 3 to 6)
+{}
+Which player should start? (enter 1 if player 'O' and 2 if player'X')
+{}
+'''.format(board_size, win_condition, player))
--- generator.sh
+++ generator.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # shellcheck disable=SC2034
-STARTING_PLAYER=${1:-"O"}
-BOARD_SIZE=${2:-"3"}
-WIN_CONDITION=${3:-"3"}
+BOARD_SIZE=${1:-"3"}
+WIN_CONDITION=${2:-"3"}
+STARTING_PLAYER=${3:-"O"}
 LINE_TYPE=${4:-"ROW"}
 
 
 mvn clean package -q -Dmaven.test.skip=true | grep -v info > stale_outputs_checked
-python3 automata/generator.py $STARTING_PLAYER $BOARD_SIZE $WIN_CONDITION $LINE_TYPE
+python3 automata/generator.py $BOARD_SIZE $WIN_CONDITION $STARTING_PLAYER $LINE_TYPE
 # > automata/templates/ > automata/run_auto.py
